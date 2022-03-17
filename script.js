65669be6ca9a708981210750c193520ef4a9ef21
_dev_goyal__ <65810521+dgbkn@users.noreply.github.com>
2022-03-17 15:19:07 +0530

    adasd

---
+++ script.js
@@ -0,0 +1,35 @@
+function makeRequest(method, url) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
+}
+
+
+var baseUrl = "https://thawing-scrubland-27252.herokuapp.com/api?uri=";
+
+function getCategoryUri(catid){
+    return(baseUrl + encodeURIComponent("https://vegamovies.nl/wp-json/wp/v2/posts?_embed&categories="+catid));
+}
+
+
+async function getLatestPosts(){
+    await makeRequest("GET", url);
+}
